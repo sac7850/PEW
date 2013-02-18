1849d8311f3d3c9d59def937efd137d8b806155a
unknown <Stacy_Crochet@SG013263.tsged.com>
2013-02-17 22:58:36 -0600

    updated env

--- config/environment.rb
+++ config/environment.rb
@@ -2,22 +2,22 @@
 require File.expand_path('../application', __FILE__)
 
 if Rails.env.test?
-  ENV["SALESFORCE_USERNAME"] = "username"
-  ENV["SALESFORCE_PASSWORD"] = "password"
+  ENV["SALESFORCE_USERNAME"] = "stacy_crochet@schumachergroup.com.stacydev"
+  ENV["SALESFORCE_PASSWORD"] = "Summer11"
   ENV["SALESFORCE_SECURITY_TOKEN"] = "KYkNGSGF8VFfXwSalHdtjsLr"
   ENV["SALESFORCE_HOST"] = "test.salesforce.com"
 end
 
 if Rails.env.development?
-  ENV["SALESFORCE_USERNAME"] = "username"
-  ENV["SALESFORCE_PASSWORD"] = "password"
+  ENV["SALESFORCE_USERNAME"] = "stacy_crochet@schumachergroup.com.stacydev"
+  ENV["SALESFORCE_PASSWORD"] = "Summer11"
   ENV["SALESFORCE_SECURITY_TOKEN"] = "KYkNGSGF8VFfXwSalHdtjsLr"
   ENV["SALESFORCE_HOST"] = "test.salesforce.com"
 end
 
 if Rails.env.production?
-  ENV["SALESFORCE_USERNAME"] = "username"
-  ENV["SALESFORCE_PASSWORD"] = "password"
+  ENV["SALESFORCE_USERNAME"] = "stacy_crochet@schumachergroup.com.stacydev"
+  ENV["SALESFORCE_PASSWORD"] = "Summer11"
   ENV["SALESFORCE_SECURITY_TOKEN"] = "KYkNGSGF8VFfXwSalHdtjsLr"
   ENV["SALESFORCE_HOST"] = "test.salesforce.com"
 end
